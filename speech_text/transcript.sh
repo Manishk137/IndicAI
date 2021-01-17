4846416fe1b38f206f4c8059da6d8f20ab0ac561
Kamlesh Joshi <kamlesh.joshi@yahoo.co.in>
2021-01-17 21:13:00 +0530

    added folder excluding huge files deepspeech-0.9.2-models.pbmm and deepspeech-0.9.2-models.scorer

---
+++ speech_text/transcript.sh
@@ -0,0 +1,2 @@
+##deepspeech --model deepspeech-0.9.1-models.pbmm --scorer deepspeech-0.9.1-models.scorer --audio name_of_audio.wav
+deepspeech --model ../speech_text/deepspeech-0.9.2-models.pbmm --scorer ../speech_text/deepspeech-0.9.2-models.scorer --audio $1
---
+++ speech_text/vid_to_au.sh
@@ -0,0 +1,2 @@
+##python audio_extract.py video_file.mp4 audio_file.wav
+python ../speech_text/audio_extract.py $1 $2 
